3b50b395677a8e8184d785e3d675c5a5d5598d7e
Matheus Custódio <matheuscustodio2802@gmail.com>
2023-10-31 10:56:49 -0300

    criação das classes User e UserResource

---
+++ projeto/src/main/java/com/matheus/custodio/projeto/resources/UserResource.java
@@ -0,0 +1,17 @@
+package com.matheus.custodio.projeto.resources;
+
+import com.matheus.custodio.projeto.entities.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/users")
+public class UserResource {
+    @GetMapping
+    public ResponseEntity<User> findAll(){
+        User u = new User(1L, "marcos", "aaa@aaa", "999999", "asdfsdfsf");
+        return ResponseEntity.ok().body(u);
+    }
+}
