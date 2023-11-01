1b4c98473eb1fdbca2141721bbc1fad87038048e
Matheus Custódio <matheuscustodio2802@gmail.com>
2023-11-01 10:18:54 -0300

    adicinada a camada de serviços da classe user e registro de componente.

--- projeto/src/main/java/com/matheus/custodio/projeto/resources/UserResource.java
+++ projeto/src/main/java/com/matheus/custodio/projeto/resources/UserResource.java
@@ -1,17 +1,29 @@
 package com.matheus.custodio.projeto.resources;
 
 import com.matheus.custodio.projeto.entities.User;
+import com.matheus.custodio.projeto.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value="/users")
 public class UserResource {
+    @Autowired
+    private UserService userService;
     @GetMapping
-    public ResponseEntity<User> findAll(){
-        User u = new User(1L, "marcos", "aaa@aaa", "999999", "asdfsdfsf");
-        return ResponseEntity.ok().body(u);
+    public ResponseEntity<List<User>> findAll(){
+        List<User> users = userService.findAll();
+        return ResponseEntity.ok().body(users);
+    }
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findByid(@PathVariable Long id){
+        User obj = userService.findByid(id);
+        return ResponseEntity.ok().body(obj);
     }
 }
